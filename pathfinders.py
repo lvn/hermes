ee6ae432ddd2bf2ddf5d243a4e3a723e4ac745f7
Elvin Yung <elvin@eventmobi.com>
2015-01-28 19:00:06 -0500

    remove redefiniton of graph

--- pathfinders.py
+++ pathfinders.py
@@ -26,15 +26,15 @@ heuristic_fns = {
 # helper functions
 
 # check if the tile at the position on the graph is passable (an integer)
-def position_passable(graph, position):
-    width = len(graph[0])
-    height = len(graph)
-    return position[0] >= 0 and position[1] >= 0 and position[0] < width and position[1] < height and graph[position[1]][position[0]]
+def position_passable(g, position):
+    width = len(g[0])
+    height = len(g)
+    return position[0] >= 0 and position[1] >= 0 and position[0] < width and position[1] < height and g[position[1]][position[0]]
 
 # get the available neighbors of position
-def get_neighbors(graph, position):
+def get_neighbors(g, position):
     (x,y) = position
-    return [item for item in [(x-1,y),(x+1,y),(x,y-1),(x,y+1)] if position_passable(graph,item)]
+    return [item for item in [(x-1,y),(x+1,y),(x,y-1),(x,y+1)] if position_passable(g,item)]
 
 # reconstructs path from a came_from dict, a startpoint, and an endpoint
 def reconstruct_path(came_from, start, end):
@@ -49,9 +49,9 @@ def reconstruct_path(came_from, start, end):
     return path
 
 # standard BFS from startpoint to endpoint. 
-def breadth_first_search(graph, start, end):
+def breadth_first_search(g, start, end):
     frontier = Queue.Queue()
-    if position_passable(graph, start): 
+    if position_passable(g, start): 
         frontier.put(start)
         came_from = {start:None}
     else:
@@ -64,7 +64,7 @@ def breadth_first_search(graph, start, end):
         if current == end: # if so, return path
             return reconstruct_path(came_from, start, end)
 
-        neighbors = get_neighbors(graph, current)
+        neighbors = get_neighbors(g, current)
         for neighbor in neighbors:
             if neighbor not in came_from:
                 frontier.put(neighbor)
@@ -73,16 +73,16 @@ def breadth_first_search(graph, start, end):
 
 # dijkstra's algorithm
 # like standard BFS, but uses a priority queue instead of a normal queue to find paths by cost
-def dijkstra(graph, start, end):
+def dijkstra(g, start, end):
     frontier = Queue.PriorityQueue()
-    if position_passable(graph, start): 
+    if position_passable(g, start): 
         frontier.put((0,start))
         came_from = {start:None}
         tentative_dist = {start:0}
     else:
         raise Exception("invalid starting position")
 
-    if not position_passable(graph, end):
+    if not position_passable(g, end):
         raise Exception("invalid end position")
 
     while not frontier.empty():
@@ -92,8 +92,8 @@ def dijkstra(graph, start, end):
         if current == end: # if so, return path
             return reconstruct_path(came_from, start, end)
 
-        for neighbor in get_neighbors(graph, current):
-            new_dist = tentative_dist[current] + graph[neighbor[1]][neighbor[0]]
+        for neighbor in get_neighbors(g, current):
+            new_dist = tentative_dist[current] + g[neighbor[1]][neighbor[0]]
             if neighbor not in tentative_dist or tentative_dist[neighbor] > new_dist:
                 frontier.put((new_dist, neighbor))
                 tentative_dist[neighbor] = new_dist
@@ -102,19 +102,19 @@ def dijkstra(graph, start, end):
 
 # greedy version of best-first search
 # uses heuristic manhattan distance as priority
-def greedy_best_first_search(graph, start, end, heuristic='default'):
+def greedy_best_first_search(g, start, end, heuristic='default'):
     if heuristic not in heuristic_fns:
         heuristic = 'default'
     heuristic = heuristic_fns[heuristic]
 
     frontier = Queue.PriorityQueue()
-    if position_passable(graph, start): 
+    if position_passable(g, start): 
         frontier.put((0,start))
         came_from = {start:None}
     else:
         raise Exception("invalid starting position")
 
-    if not position_passable(graph, end):
+    if not position_passable(g, end):
         raise Exception("invalid end position")
 
     while not frontier.empty():
@@ -124,7 +124,7 @@ def greedy_best_first_search(graph, start, end, heuristic='default'):
         if current == end: # if so, return path
             return reconstruct_path(came_from, start, end)
 
-        for neighbor in get_neighbors(graph, current):
+        for neighbor in get_neighbors(g, current):
             if neighbor not in came_from:
                 heuristic_dist = heuristic(neighbor,end)
                 frontier.put((heuristic_dist, neighbor))
@@ -133,20 +133,20 @@ def greedy_best_first_search(graph, start, end, heuristic='default'):
 
 # A*
 # basically combines dijkstra and greedy bfs
-def a_star(graph, start, end, heuristic='default'):
+def a_star(g, start, end, heuristic='default'):
     if heuristic not in heuristic_fns:
         heuristic = 'default'
     heuristic = heuristic_fns[heuristic]
 
     frontier = Queue.PriorityQueue()
-    if position_passable(graph, start): 
+    if position_passable(g, start): 
         frontier.put((0,start)) # aka open set
         came_from = {start:None} # doubles as the closed set
         past_dist = {start:0} # aka the past score g(x)
     else:
         raise Exception("invalid starting position")
 
-    if not position_passable(graph, end):
+    if not position_passable(g, end):
         raise Exception("invalid end position")
 
     while not frontier.empty():
@@ -156,9 +156,9 @@ def a_star(graph, start, end, heuristic='default'):
         if current == end: # if so, return path
             return reconstruct_path(came_from, start, end)
 
-        for neighbor in get_neighbors(graph, current):
+        for neighbor in get_neighbors(g, current):
             # calculate tentative overall distance
-            tentative_past_dist = past_dist[current] + graph[neighbor[1]][neighbor[0]]
+            tentative_past_dist = past_dist[current] + g[neighbor[1]][neighbor[0]]
             if neighbor not in came_from or past_dist[neighbor] > tentative_past_dist:
                 # insert into frontier by priority of lower overall_tentative_dist
                 heuristic_dist = heuristic(neighbor,end)
